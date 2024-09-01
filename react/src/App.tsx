7bb319f5d28fb4e82032bc118b96e57bde97b2a7
Gilberts Ahumada <gilbertsahumada@gmail.com>
2024-09-01 18:26:52 -0300

    video 9 done

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import AppRoutes from './components/AppRoutes';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Router>
