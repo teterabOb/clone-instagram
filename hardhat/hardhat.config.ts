75b8d0e064722f92a4ca2ae146c8b5786d9de7d6
Gilberts Ahumada <gilbertsahumada@gmail.com>
2024-09-01 12:00:12 -0300

    updating readme file and config to deploy contracts

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,9 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
-const { vars } = require("hardhat/config");
+import dotenv from "dotenv";
+dotenv.config();
+//const { vars } = require("hardhat/config");
 
-const AVAX_PRIVATE_KEY = vars.get("AVAX_PRIVATE_KEY");
+// npx hardhat vars set AVAX_PRIVATE_KEY 
+// const AVAX_PRIVATE_KEY = vars.get("AVAX_PRIVATE_KEY");
+const AVAX_PRIVATE_KEY = `${process.env.AVAX_PRIVATE_KEY}` || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
